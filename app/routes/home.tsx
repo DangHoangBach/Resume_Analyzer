76fb932ea8468c75b5ce765805b64179536a1410
Dang Hoang Bach <danghoangbach2608@gmail.com>
2026-04-18 23:49:54 +0700

    feat: code phần homepage, 2 components navbar và Resume card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
+import Navbar from "~/components/navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "Welcome to your new dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track your Application and Resume</h1>
+        <h2>Review your submission and check your feedback</h2>
+      </div>
+
+      {resumes.length > 0 && ( // && vế >0 đúng, vế sau mới chạy
+        <div className="resumes-section">
+          {resumes.map((resume) => ( // map từ resumes từ cái kho constants ấy
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+
+
+
+
+  </main>
+
 }
